69494e98193e70ba45dd424d76fc057f14808b6a
oleksharh <oleksharh@gmail.com>
2024-12-07 11:38:28 +0000

    initial commit

---
+++ config.h
@@ -0,0 +1,14 @@
+#define TFT_RST  20
+#define TFT_DC   21
+#define TFT_CS   19
+#define TFT_WR   17
+#define TFT_RD   16
+
+#define TFT_D0   5
+#define TFT_D1   4
+#define TFT_D2   3
+#define TFT_D3   2
+#define TFT_D4   15
+#define TFT_D5   14
+#define TFT_D6   13
+#define TFT_D7   12
